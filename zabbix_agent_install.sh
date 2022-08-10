753d73669716f9a4d2731b9f4c770f892d57145b
PratikPandey711 <99240399+pratikpandey711@users.noreply.github.com>
2022-08-10 13:48:37 +0530

    changes Made

--- zabbix_agent_install.sh
+++ zabbix_agent_install.sh
@@ -7,11 +7,13 @@ fi
 
 # Only run it if we can (ie. on Ubuntu/Debian)
 if [ -x /usr/bin/apt-get ]; then
+  wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-3+debian11_all.deb
+  dpkg -i zabbix-release_6.0-3+debian11_all.deb
   apt-get update
   apt-get -y install zabbix-agent sysv-rc-conf
   sysv-rc-conf zabbix-agent on
-  sed -i 's/Server=127.0.0.1/Server=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=127.0.0.1/ServerActive=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/Server=127.0.0.1/Server=192.168.56.103/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/ServerActive=127.0.0.1/ServerActive=192.168.56.103/' /etc/zabbix/zabbix_agentd.conf
   HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
   service zabbix-agent restart
 fi
@@ -19,11 +21,11 @@ fi
 # Only run it if we can (ie. on RHEL/CentOS)
 if [ -x /usr/bin/yum ]; then
   yum -y update
-  rpm -ivh http://repo.zabbix.com/zabbix/2.4/rhel/6/x86_64/zabbix-release-2.4-1.el6.noarch.rpm
+  rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-2.el8.noarch.rpm
   yum -y install zabbix-agent
   chkconfig zabbix-agent on
-  sed -i 's/Server=127.0.0.1/Server=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=127.0.0.1/ServerActive=52.90.49.206/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/Server=127.0.0.1/Server=192.168.56.103/' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/ServerActive=127.0.0.1/ServerActive=192.168.56.103/' /etc/zabbix/zabbix_agentd.conf
   HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
   service zabbix-agent restart
 fi
